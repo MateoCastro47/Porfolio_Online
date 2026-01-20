9633151c35a5443001853ef1bb6569483672dfcf
MateoCastro47 <castrosoutomateo@gmail.com>
2026-01-20 12:40:46 +0100

    Creado el skillsComponent junto a sus estilos, añadido al app

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import HeaderComponent from './components/HeaderComponent'
 import HeroComponent from './components/HeroComponent'
 import AboutComponent from './components/AboutComponent';
+import SkillsComponent from './components/SkillsComponent';
 function App() {
 
   return (
     <>
-    <HeaderComponent></HeaderComponent>
-    <HeroComponent></HeroComponent>
-    <AboutComponent></AboutComponent>
+      <HeaderComponent></HeaderComponent>
+      <HeroComponent></HeroComponent>
+      <AboutComponent></AboutComponent>
+      <SkillsComponent></SkillsComponent>
     </>
   )
 }
